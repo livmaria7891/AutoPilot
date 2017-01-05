7f5feb20ffb7b46cef4bc2194408eb65de298619
Olivia <reach.olivialegge@gmail.com>
2017-01-05 13:58:55 -0800

    added table view for all flights and cell view controller

---
+++ AutoPilot/AutoPilot/FlightTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FlightTableViewCell.swift
+//  AutoPilot
+//
+//  Created by Olivia Legge on 1/5/17.
+//  Copyright © 2017 Olivia Legge. All rights reserved.
+//
+
+import UIKit
+
+class FlightTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
