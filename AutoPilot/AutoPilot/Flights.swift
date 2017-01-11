4d0f978d86ec260700e8af834b9f7ae1b79c18f9
Olivia <reach.olivialegge@gmail.com>
2017-01-11 12:12:02 -0800

    added foundation for persisting data

--- AutoPilot/AutoPilot/Flights.swift
+++ AutoPilot/AutoPilot/Flights.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import os.log
 
-class Flight {
+class Flight: NSObject, NSCoding {
     
     //MARK: Properties
     
@@ -16,7 +17,22 @@ class Flight {
     var steps: [String]?
     var supplies: [String]?
     var isFavorite: Bool
-    var favImage: UIImage?
+//    var favImage: UIImage?
+    
+    //MARK: Archiving Paths
+    
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("flights")
+    
+    //MARK: Types
+    
+    struct PropertyKey {
+        static let name = "name"
+        static let steps = "steps"
+        static let supplies = "supplies"
+        static let isFavorite = "isFavorite"
+
+    }
     
     //MARK: Initialization
     
@@ -31,13 +47,46 @@ class Flight {
         self.name = name
         self.isFavorite = isFavorite
         
-        addFavoriteImage()
+//        addFavoriteImage()
     }
     
-    func addFavoriteImage(){
-        if self.isFavorite == true {
-            favImage = UIImage(named: "placeHolderStar")
-        }
+//    func addFavoriteImage(){
+//        if self.isFavorite == true {
+//            favImage = UIImage(named: "placeHolderStar")
+//        }
+//    }
+    
+    //MARK: NSCoding
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(steps, forKey: PropertyKey.steps)
+        aCoder.encode(supplies, forKey: PropertyKey.supplies)
+        aCoder.encode(isFavorite, forKey: PropertyKey.isFavorite)
+        
     }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        
+        // The name is required. If we cannot decode a name string, the initializer should fail.
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Flight object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        let steps = aDecoder.decodeObject(forKey: PropertyKey.steps) as? [String]
+        
+        let supplies = aDecoder.decodeObject(forKey: PropertyKey.supplies) as? [String]
+        
+        guard let isFavorite = aDecoder.decodeObject(forKey: PropertyKey.isFavorite) as? Bool else {
+            os_log("Unable to decode isFavorite for a Flight object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        
+        // Must call designated initializer.
+        self.init(name: name, steps: steps, supplies: supplies, isFavorite: isFavorite)
+    }
+
     
 }
